4db033da98dc99fcd4adeb141940aed7af5badc5
VIGNESH P <mrvignesh44@gmail.com>
2025-07-23 10:40:07 +0530

    Update ProductController.java

--- src/main/java/com/ecom/E_com_Project/Controller/ProductController.java
+++ src/main/java/com/ecom/E_com_Project/Controller/ProductController.java
@@ -23,7 +23,7 @@ import com.ecom.E_com_Project.Model.Product;
 import com.ecom.E_com_Project.Service.ProductService;
 
 @RestController
-@CrossOrigin(origins = "https://ff-update-backend-6.onrender.com")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class ProductController {
 	@Autowired
