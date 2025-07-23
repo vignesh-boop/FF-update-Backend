9ff3908e6d15ae85e1387ab39815caefea021847
VIGNESH P <mrvignesh44@gmail.com>
2025-07-23 20:12:08 +0530

    Update Product.java

--- src/main/java/com/ecom/E_com_Project/Model/Product.java
+++ src/main/java/com/ecom/E_com_Project/Model/Product.java
@@ -1,28 +1,18 @@
 package com.ecom.E_com_Project.Model;
 
 import java.math.BigDecimal;
-
-
+import java.sql.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import java.sql.Date;
-
 import lombok.NoArgsConstructor;
 
 @Entity
-
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -35,12 +25,9 @@ public class Product {
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date releaseDate;
-    
-     
-    
-    
-    
+
     private int stockQuantity;
+    private boolean productAvailable;
 
     private String imageName;
     private String imageType;
@@ -48,97 +35,40 @@ public class Product {
     @Lob
     private byte[] imageData;
 
-    // Manually overridden getters and setters
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public int getId() {
-        return id;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-    public String getName() {
-        return name;
-    }
+    public String getBrand() { return brand; }
+    public void setBrand(String brand) { this.brand = brand; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
 
-    public String getDescription() {
-        return description;
-    }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public Date getReleaseDate() { return releaseDate; }
+    public void setReleaseDate(Date releaseDate) { this.releaseDate = releaseDate; }
 
-    public String getBrand() {
-        return brand;
-    }
+    public int getStockQuantity() { return stockQuantity; }
+    public void setStockQuantity(int stockQuantity) { this.stockQuantity = stockQuantity; }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
+    public boolean isProductAvailable() { return productAvailable; }
+    public void setProductAvailable(boolean productAvailable) { this.productAvailable = productAvailable; }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
+    public String getImageName() { return imageName; }
+    public void setImageName(String imageName) { this.imageName = imageName; }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+    public String getImageType() { return imageType; }
+    public void setImageType(String imageType) { this.imageType = imageType; }
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Date getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(Date releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-   
-
-   
-
-    public int getStockQuantity() {
-        return stockQuantity;
-    }
-
-    public void setStockQuantity(int stockQuantity) {
-        this.stockQuantity = stockQuantity;
-    }
-
-    public String getImageName() {
-        return imageName;
-    }
-
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
-
-    public String getImageType() {
-        return imageType;
-    }
-
-    public void setImageType(String imageType) {
-        this.imageType = imageType;
-    }
-
-    public byte[] getImageData() {
-        return imageData;
-    }
-
-    public void setImageData(byte[] imageData) {
-        this.imageData = imageData;
-    }
+    public byte[] getImageData() { return imageData; }
+    public void setImageData(byte[] imageData) { this.imageData = imageData; }
 }
