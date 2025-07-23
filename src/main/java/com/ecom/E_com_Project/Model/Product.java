a81f739b6a63129c0eace1235a36ae805902d072
VIGNESH P <mrvignesh44@gmail.com>
2025-07-23 14:59:32 +0530

    Update Product.java

--- src/main/java/com/ecom/E_com_Project/Model/Product.java
+++ src/main/java/com/ecom/E_com_Project/Model/Product.java
@@ -15,105 +15,129 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	
 
-	private int id;
-	private String name;
-	private String description;
-	private String brand;
-	private BigDecimal price;
-	private String category;
-	
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private Date releaseDate;
-	private boolean productAvailable;
-	private int stockQuantity;
-	private String imageName;
-	private String imageType;
-	@Lob
-	private byte[] imageData;
-	
-	
-	public String getImageName() {
-		return imageName;
-	}
-	public void setImageName(String imageName) {
-		this.imageName = imageName;
-	}
-	public String getImageType() {
-		return imageType;
-	}
-	public void setImageType(String imageType) {
-		this.imageType = imageType;
-	}
-	public byte[] getImageData() {
-		return imageData;
-	}
-	public void setImageData(byte[] imageData) {
-		this.imageData = imageData;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getBrand() {
-		return brand;
-	}
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-	public BigDecimal getPrice() {
-		return price;
-	}
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public Date getReleaseDate() {
-		return releaseDate;
-	}
-	public void setReleaseDate(Date releaseDate) {
-		this.releaseDate = releaseDate;
-	}
-	public boolean isProductAvailable() {
-		return productAvailable;
-	}
-	public void setProductAvailable(boolean available) {
-		this.productAvailable = available;
-	}
-	public int getstockQuantity() {
-		return stockQuantity;
-	}
-	public void setstockQuantity(int stockQuantity) {
-		this.stockQuantity = stockQuantity;
-	}
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date releaseDate;
+
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
+    // Manually overridden getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isProductAvailable() {
+        return productAvailable;
+    }
+
+    public void setProductAvailable(boolean productAvailable) {
+        this.productAvailable = productAvailable;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
 }
